63ea29c3955e6a3745d31a8d9a32f84771ab3919
Manojit Paul <manojitp4@gmail.com>
2017-02-05 00:34:01 +0530

    codechef FeB

---
+++ java_workspace/CodeChef/src/codechef/KBIGNUMB.java
@@ -0,0 +1,22 @@
+package codechef;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class KBIGNUMB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int test = sc.nextInt();
+		for(int tt=0;tt<test;tt++)
+		{
+			int a = sc.nextInt();
+			long n  = sc.nextLong();
+			int m = sc.nextInt();
+			
+				
+		}
+	}
+
+}
--- java_workspace/CodeChef/src/codechef/Solution.java
+++ java_workspace/CodeChef/src/codechef/Solution.java
@@ -1,73 +1,45 @@
 package codechef;
 
-import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class Solution {
-	static long[][][] matrix;
-	static HashMap<String,Long> hashMap = new HashMap<String,Long>();
-	
-	static void update(int x , int y, int z, long w){
-		matrix[x][y][z] = w;
-		hashMap.put(x+"-"+y+"-"+z, w);
-	}
-	
-	static void query(int x1 , int y1, int z1, int x2 , int y2, int z2){
-		long sum = 0;
-		Set set = hashMap.entrySet();
-		Iterator iterator = set.iterator();
-		while(iterator.hasNext()){
-			Map.Entry mentry = (Map.Entry)iterator.next();
-			String[] value = mentry.getKey().toString().split("-");
-			int x = Integer.parseInt(value[0]);
-			int y = Integer.parseInt(value[1]);
-			int z = Integer.parseInt(value[2]);
-			if(((x<=x2 && x>=x1) && (y<=y2 && y>=y1) && (z<=z2 && z>=z1)) || ((x<=x1 && x>=x2) && (y<=y1 && y>=y2) && (z<=z1 && z>=z2))){
-				sum = sum + Long.parseLong(mentry.getValue().toString());
-			}
-		}
-		System.out.println(sum);
-	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
 		Scanner sc = new Scanner(System.in);
-		int test = sc.nextInt();
-		for(int i=0;i<test;i++){
-			hashMap.clear();
-			int size = sc.nextInt();
-			size = size +1;
-			int operartion = sc.nextInt();
-			matrix = new long[size][size][size];
-			sc.nextLine();
-			for(int j=0;j<operartion;j++){
-				
-				String l = sc.nextLine();
-				String[] a = l.split(" ");
-				String queries = a[0];
-				//System.out.println(queries+"hjbbjb");
-				
-				if(queries.toLowerCase().equals("update")){
-					int x = Integer.parseInt(a[1]);
-					int y = Integer.parseInt(a[2]);
-					int z = Integer.parseInt(a[3]);
-					long w = Long.parseLong(a[4]);
-					//System.out.println(x+" "+y+" "+z+" "+w);
-					update(x,y,z,w);
-				}
-				else{
-					int x1 = Integer.parseInt(a[1]);
-					int y1 = Integer.parseInt(a[2]);
-					int z1 = Integer.parseInt(a[3]);
-					int x2 = Integer.parseInt(a[4]);
-					int y2 = Integer.parseInt(a[5]);
-					int z2 = Integer.parseInt(a[6]);
-					//System.out.println(x1+" "+y1+" "+z1+" "+x2);
-					query(x1,y1,z1,x2,y2,z2);
+		int n = sc.nextInt();
+		long l = sc.nextLong();
+		long r = sc.nextLong();
+		long[] num = new long[n];
+		for(int i=0;i<n;i++)
+		{
+			num[i] = sc.nextLong();
+		}
+		Arrays.sort(num);
+		
+		long max = num[n-1]+num[n-2];
+		if(r<max) max =r;
+		else  max = max-1;
+		
+		long min = max;
+		
+		for(int i=n-1;i>0;i--)
+		{
+			long x = (num[i]-num[i-1]);
+			long y = (num[i]+num[i-1]);
+			if(x<min)
+			{
+				if(y>=min)
+				{
+					min = y;
+					if(l>min) min = l;
+					else  min = min+1;
 				}
 			}
 		}
+
 		
 	}
 
